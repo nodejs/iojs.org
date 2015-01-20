9b489a2eff10f11b5fc605ccaa762b28ab0f3720
Fishrock123 <fishrock123@rocketmail.com>
2015-01-20 17:39:53 -0500

    use postcss for css autoprefixing and minification

--- gulp/tasks/minifyCss.js
+++ gulp/tasks/minifyCss.js
@@ -1,11 +1,16 @@
 var gulp      = require('gulp');
 var config    = require('../config');
-var minifyCSS = require('gulp-minify-css');
+var postcss   = require('gulp-postcss');
+var autoprefixer = require('autoprefixer-core');
+var csswring  = require('csswring');
 var size      = require('gulp-filesize');
 
 gulp.task('minifyCss', function() {
   return gulp.src(config.cssSrc)
-    .pipe(minifyCSS({keepBreaks:true}))
+    .pipe(postcss([
+      autoprefixer(),
+      csswring
+    ]))
     .pipe(gulp.dest(config.dest))
     .pipe(size());
 })
