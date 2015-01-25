cbe7ef02d65b914a5cf1fbcaa56de5cc3cfc53c8
Trent Oswald <trentoswald@therebelrobot.com>
2015-01-25 13:05:30 -0700

    [minor] removed unneeded object holders, added in noDuplicate param for if Substack merges my commit over there.

--- gulp/tasks/templates.js
+++ gulp/tasks/templates.js
@@ -8,8 +8,6 @@ var md = require('markdown-it')(); // to convert markdown to html
 var source = require('vinyl-source-stream'); // used to convert substack's readStream to vinylStream
 
 gulp.task('templates', function() {
-  var htmlObjHolder = {}; // these holders are needed later to keep write streams separate
-  var i18nObjHolder = {};
   var contentFiles = glob.sync(config.contentSrc); // read in all content files in the repo
 
   var languages = _.uniq(_.map(contentFiles, function(str) { // extrapolate the languages from the content filepaths
@@ -17,8 +15,6 @@ gulp.task('templates', function() {
   }));
 
   _.forEach(languages, function(lang) { // loop through languages to make separate folder outputs
-    htmlObjHolder[lang] = {}; // expanding the stream holders for later
-    i18nObjHolder[lang] = {};
     var templateJSON = require('../../content/' + lang + '/template.json'); // read in the template JSON file
 
     var templateFiles = _.where(contentFiles, function(str) { // return list of content files in this language alone
@@ -38,16 +34,16 @@ gulp.task('templates', function() {
       var html = md.render(markdown); // convert md string to html string
       var thisFileJSON = _.cloneDeep(templateJSON); // clone in the template JSON object
       thisFileJSON['[i18n-content]'] = html; // Attach md2html string to the interpolation object
-      htmlObjHolder[lang][file.filename] = HTMLtemplate(); // finally using that holder for the template stream
-      i18nObjHolder[lang][file.filename] = htmlObjHolder[lang][file.filename].template('i18n'); // same
+      var htmlObj = HTMLtemplate(); // finally using that holder for the template stream
+      i18nObj = htmlObj.template('i18n',true); // same
       var filepath = __dirname.split('gulp/tasks')[0] + 'source/templates/test.html'; // get the main template file location. There can be multiple, this is just a proof of concept
 
       fs.createReadStream(filepath) // pulling this code from substack's example on html-template
-      .pipe(htmlObjHolder[lang][file.filename])
+      .pipe(htmlObj)
         .pipe(source(file.filename + '.html')) // converting the readStream to a vinyl stream so gulp can write it back to the disk
       .pipe(gulp.dest('public_test/' + lang + '/')); // dump it in the appropriate language subfolder
-      i18nObjHolder[lang][file.filename].write(thisFileJSON); // write the interpolation JSON to the template
-      i18nObjHolder[lang][file.filename].end(); // saving? this is taken from substack too.
+      i18nObj.write(thisFileJSON); // write the interpolation JSON to the template
+      i18nObj.end(); // saving? this is taken from substack too.
     });
   });
 });
