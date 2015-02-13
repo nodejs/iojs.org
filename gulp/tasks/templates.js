5dd320d60c613cde4ef563b9c0754a59d99cfd57
Anton Wilhelm <timaschew@gmail.com>
2015-02-13 22:02:37 +0100

    use jade instead of html-templates

--- gulp/tasks/templates.js
+++ gulp/tasks/templates.js
@@ -1,14 +1,13 @@
 var _ = require('lodash'); // to handle collections gracefully
 var config = require('../config').templates; // pull in the pathing config file
 var fs = require('fs'); // used to work with substack's module
+var path = require('path'); // use for get dirname of a path
 var glob = require('glob'); // to dynamically read in all content md files
 var gulp = require('gulp'); // because this is a gulp task. duh.
-var HTMLtemplate = require('html-template'); // substack's html template implementation
+var jade = require('gulp-jade'); // to render jade to html.
+var rename = require('gulp-rename'); // to use different file name between input and output
 var md = require('markdown-it')(); // to convert markdown to html
-var source = require('vinyl-source-stream'); // used to convert substack's readStream to vinylStream
-var replaceStream = require('replacestream'); // used to add an element to the html: making sure each page has it's own class if needed in css
 var utils = require('../util/template-utils.js');
-var path = require('path');
 
 gulp.task('templates', function() {
   var contentFiles = glob.sync(config.contentSrc); // read in all content files in the repo
@@ -24,27 +23,22 @@ gulp.task('templates', function() {
     _.forEach(markdownFilesInThisLang, function(file) { // iterate over the md files present in this language to apply the template to them
       var markdown = String(fs.readFileSync(file.srcPath)); // read in the md file, convert buffer to string
       var html = md.render(markdown); // convert md string to html string
-      var thisFileJSON = _.cloneDeep(templateJSON); // clone in the template JSON object
-      var pageTitle = thisFileJSON['browser-title'];
-      thisFileJSON = _.omit(thisFileJSON,'browser-title');
-      var finalJSON = {};
-      _.forEach(thisFileJSON, function(value, key){
-        finalJSON['[i18n-'+key+']'] = value;
-      })
-      finalJSON['[i18n-content]'] = html; // Attach md2html string to the interpolation object
-      var htmlObj = HTMLtemplate(); // finally using that holder for the template stream
-      i18nObj = htmlObj.template('i18n',{include:false}); // same
-      var filepath = __dirname.split('gulp/tasks')[0] + 'source/templates/main.html'; // get the main template file location. There can be multiple, this is just a proof of concept
-      var destinationDirectory = path.dirname('public/' + file.filepathArray.join('/'));
-      var fileStream = fs.createReadStream(filepath) // pulling this code from substack's example on html-template
-        .pipe(replaceStream('<title i18n-title>io.js - JavaScript I/O</title>','<title i18n-title>'+pageTitle+'</title>'))
-        .pipe(replaceStream('markdown-page=""', 'markdown-page="'+file.filename+'"')) // add css-triggerable attribute to body
-        .pipe(replaceStream('[page-stylesheet]',file.filename)) // require in specific stylesheet
-        .pipe(htmlObj)
-        .pipe(source(file.filename + '.html')) // converting the readStream to a vinyl stream so gulp can write it back to the disk
-        .pipe(gulp.dest(destinationDirectory)); // dump it in the appropriate language subfolder
-      i18nObj.write(finalJSON); // write the interpolation JSON to the template
-      i18nObj.end(); // saving? this is taken from substack too.
-    });
+
+      templateJSON.page = file.filename; // extend locals for special styles on each page
+      templateJSON['page-stylesheet'] = file.filename; // for special css files for the page
+      templateJSON.lang = lang; // extend to provide html lang attribute into the template
+      templateJSON['i18n-content'] = html; // attach the rendered markdown into the body
+
+      var filepath = __dirname.split('gulp/tasks')[0] + 'source/templates/main.jade'; // get the main template file location. There can be multiple, this is just a proof of concept
+      var destinationDirectory = path.dirname('public/' + file.filepathArray.join('/')); // consider 
+
+      gulp.src(filepath) // read jade template
+      .pipe(jade({ // render template while passing locals
+        locals: templateJSON
+      }))
+      .pipe(rename(file.filename + '.html')) // rename output file, using md filename 
+      .pipe(gulp.dest(destinationDirectory)); // dump it in the appropriate language subfolder
+
+     });
   });
 });
