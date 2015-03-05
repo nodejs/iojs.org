464314369f1c6638f44db229a4098489702569d6
Antonis Karamitros <ablackjim228@gmail.com>
2015-03-05 20:18:02 +0000

    [gulp-tasks]
    - add comments on the tasks when printing them

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -5,10 +5,10 @@ gulp.task('default', function(){
   var message =
   '---- io.js website ----\n' +
   'gulp tasks available:\n'   +
-  '- develop\n' +
-  '- build\n'   +
-  '- server\n'  +
-  '- clean\n';
+  '- develop // Runs the `build` and `server` tasks and also keeps a watcher running\n' +
+  '- build   // build the HTML and CSS files\n'   +
+  '- server  // start the local dev server\n'  +
+  '- clean   // clean out directory\n';
 
   console.log(message);
 });
