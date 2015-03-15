ef05dd17becc5ca3a08ed994d5d042a546b0483d
Sean Ouimet <sean@seanouimet.com>
2015-03-15 13:18:51 +0100

    Adds “download” to build, develop tasks

--- gulp/tasks/develop.js
+++ gulp/tasks/develop.js
@@ -4,6 +4,7 @@ var runSequence = require('run-sequence');
 gulp.task('develop', function(cb){
   runSequence(
     'clean',
+    'download',
     'content',
     ['stylus', 'templates'],
     ['watch', 'server'],
