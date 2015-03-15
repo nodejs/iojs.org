4b541967af113f4a81fc98414c88c53caf2672fc
Sean Ouimet <sean@seanouimet.com>
2015-03-15 14:11:00 +0100

    download: proof-of-concept, bring in external .md content

--- gulp/tasks/download.js
+++ gulp/tasks/download.js
@@ -3,9 +3,32 @@ var fs = require('fs');
 var path = require('path');
 var request = require('request');
 
+var manifest = {
+  "content/en/governance.md": "https://raw.githubusercontent.com/iojs/io.js/v1.x/GOVERNANCE.md",
+  "content/en/working-groups.md": "https://raw.githubusercontent.com/iojs/io.js/v1.x/WORKING_GROUPS.md",
+  "source/versions.json": "https://iojs.org/dist/index.json"
+}
+
 gulp.task('download', function () {
-  var dir = path.resolve(__dirname, '..', '..', 'source');
-  var outputFile = path.resolve(dir, 'versions.json');
-  console.log('updating source/versions.json');
-  return request('https://iojs.org/dist/index.json').pipe(fs.createWriteStream(outputFile));
+  var dir = path.resolve(__dirname, '..', '..');
+  var streams = [];
+  var targets = Object.keys(manifest);
+
+  targets.forEach(function(target) {
+    console.log('updating', target);
+
+    var source = manifest[target];
+    var outputFile = path.resolve(dir, target);
+
+    var promise = new Promise(function(resolve, reject) {
+      var outStream = fs.createWriteStream(outputFile);
+      outStream.on('error', reject);
+      outStream.on('finish', resolve);
+      var stream = request(source).pipe(outStream);
+    });
+
+    streams.push(promise);
+  })
+
+  return Promise.all(streams);
 });
