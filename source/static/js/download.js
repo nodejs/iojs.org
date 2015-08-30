1d81373aadf77ca522f028bdb5151dd8746c932b
Frederic Hemberger <mail@frederic-hemberger.de>
2015-08-30 11:19:59 +0200

    Optimize download.js

--- source/static/js/download.js
+++ source/static/js/download.js
@@ -1,27 +1,25 @@
 ;
-(function(d, n) {
+(function (d, n) {
+  'use strict';
   var os = n.platform.match(/(Win|Mac|Linux)/);
   var x = n.userAgent.match(/x86_64|Win64|WOW64/) ||
     n.cpuClass === 'x64' ? 'x64' : 'x86';
-  var base = 'https://iojs.org/dist/latest/';
   var db = d.getElementById('home-downloadbutton');
   if (!db) { return; }
   var version = db.dataset.version;
   var dlLocal = db.dataset.dlLocal;
-  var href = ''; //If we can't match, just link it directly to the download list.
   switch (os && os[1]) {
     case 'Mac':
-      href = 'iojs-' + version + '.pkg';
-      db.innerHTML = dlLocal + ' OS X (x64)';
+      db.href += 'iojs-' + version + '.pkg';
+      db.innerText = dlLocal + ' OS X (x64)';
       break;
     case 'Win':
-      href = 'iojs-' + version + '-' + x + '.msi';
-      db.innerHTML = dlLocal + ' Windows ('+x+')';
+      db.href += 'iojs-' + version + '-' + x + '.msi';
+      db.innerText = dlLocal + ' Windows (' + x + ')';
       break;
     case 'Linux':
-      href = 'iojs-' + version + '-linux-' + x + '.tar.gz';
-      db.innerHTML = dlLocal + ' Linux ('+x+')';
+      db.href += 'iojs-' + version + '-linux-' + x + '.tar.gz';
+      db.innerText = dlLocal + ' Linux (' + x + ')';
       break;
   }
-  db.href = base + href;
 })(document, navigator);
